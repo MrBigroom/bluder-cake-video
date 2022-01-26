69f73953a38df8925c02d6adaeb4474aeab8c12d
MrBigroom <blabob9@gmail.com>
2022-01-26 18:58:04 +0800

    test 1

--- script.js
+++ script.js
@@ -1,56 +1,35 @@
-const video = document.getElementById('video');
-const play = document.getElementById('play');
-const progress = document.getElementById('progress');
-const timestamp = document.getElementById('timestamp');
+AFRAME.registerComponent('screenhandler', {
+  init: function() {
+      console.log("In Init for Screen Handler");
 
-// Play & pause video
-function toggleVideoStatus() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
+      var el = this.el;
+      el.addEventListener('click', function() {
+          el.setAttribute("material", "src", "#bluder-cake");
+
+          var videoEl = document.querySelector("#bluder-cake");
+          el.setAttribute("visible", true);
+
+          console.log("Playing video");
+          videoEl.play();
+      });
   }
-}
+});
 
-// update play/pause icon
-function updatePlayIcon() {
-  if (video.paused) {
-    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
-  } else {
-    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+AFRAME.registerComponent('play-on-click', {
+  init: function() {
+      this.onClick = this.onClick.bind(this);
+  },
+  play: function() {
+      window.addEventListener('click', this.onClick);
+  },
+  pause: function() {
+      window.removeEventListener('click', this.onClick);
+  },
+  onClick: function(evt) {
+      var video = this.el.components.material.material.map.image;
+      if(!video) {
+          return;
+      }
+      video.play();
   }
-}
-
-// Update progress & timestamp
-function updateProgress() {
-  progress.value = (video.currentTime / video.duration) * 100;
-
-  // Get minutes
-  let mins = Math.floor(video.currentTime / 60);
-  if (mins < 10) {
-    mins = '0' + String(mins);
-  }
-
-  // Get seconds
-  let secs = Math.floor(video.currentTime % 60);
-  if (secs < 10) {
-    secs = '0' + String(secs);
-  }
-
-  timestamp.innerHTML = `${mins}:${secs}`;
-}
-
-// Set video time to progress
-function setVideoProgress() {
-  video.currentTime = (+progress.value * video.duration) / 100;
-}
-
-// Event listeners
-video.addEventListener('click', toggleVideoStatus);
-video.addEventListener('pause', updatePlayIcon);
-video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timeupdate', updateProgress);
-
-play.addEventListener('click', toggleVideoStatus);
-
-progress.addEventListener('change', setVideoProgress);
+});
